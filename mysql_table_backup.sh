dcabaa090bea296ac3c8d05e4951f896e8e6f28a
Darren <42687432+Bertram521@users.noreply.github.com>
2020-04-15 17:45:38 +0800

    Add files via upload

---
+++ mysql_table_backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source /etc/profile
+DATE=`date +%Y-%m-%d`
+DATE_NOW=`date +%Y%m%d.%H%M`
+old_day=$(date +%Y-%m-%d --date='5 days ago')
+SERVER=`hostname`
+
+MYSQL_DATA=/data/mysql
+BACKUP_DIR=/data/backup/mysql/
+DB_USER=backup
+DB_PASSWD=backup123456
+
+
+if [ -d $BACKUP_DIR ];
+then
+     mkdir -p $BACKUP_DIR/$DATE/jesscard
+     cd $BACKUP_DIR/$DATE
+for table in $(mysql -u${DB_USER} -p${DB_PASSWD} -h10.161.28.44  -e "show tables from jescard_data;" | egrep -w '(BrandR|Brand|Category|CategoryR|IgnoredEntitySource|Mall|RelevanceSession|SkuGood|SpuGood|LowestSku|HightestSku|GoodsLanguage|BrandLanguage)')
+do
+     mysqldump  -u${DB_USER} -p${DB_PASSWD} -h'10.112.28.124' --opt --lock-tables=false --routines --triggers --events --single-transaction --default-character-set=utf8 -f jescard_data $table | gzip >  $BACKUP_DIR/$DATE/jesscard/${table}.sql.gz
+done   
+    
+    if [ -d $BACKUP_DIR ];
+    then
+        rm -rf ${BACKUP_DIR}/${old_day}
+    else
+        echo "$SERVER mysqldump error!"> /tmp/dump.txt
+    fi	
+else
+	echo "data is not exist"
+fi
