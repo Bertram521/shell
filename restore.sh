6f9a86141ad63aa0253bef5a6830d677eb29f1b0
Darren <42687432+Bertram521@users.noreply.github.com>
2020-08-22 15:33:27 +0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -5,16 +5,6 @@ time=`date +%F`
 tables=(
 Brand
 BrandR
-Category
-CategoryR
-IgnoredEntitySource
-Mall
-RelevanceSession
-SkuGood
-SpuGood
-LowestSku
-HightestSku
-GoodsLanguage
 BrandLanguag
 )
 
@@ -23,7 +13,7 @@ do
 #gzip -d /data/backup/mysql/${time}/jesscard/${table}.[0-9]*.gz
 #mv /data/backup/mysql/${time}/jesscard/${table}.[0-9]*.sql /data/backup/mysql/${time}/jesscard/${table}.sql
 echo "==========${table} restore started at `date +%F\ %H:%M:%S`=========="  >> /data/scripts/restore.log
-gunzip < /data/backup/mysql/${time}/jesscard/${table}.sql.gz |mysql -uroot -ptest1234 -h39.116.231.18 --default-character-set=utf8 --max-allowed-packet=512M -f jescard_data 
+gunzip < /data/backup/mysql/${time}/${table}.sql.gz |mysql -uroot -ptest1234 -h10.196.85.18 --default-character-set=utf8 --max-allowed-packet=512M -f bertram_data 
 echo "==========${table} restore ended at `date +%F\ %H:%M:%S`==========" >> /data/scripts/restore.log
 #gzip /data/backup/mysql/${time}/jesscard/${table}.sql
 done
